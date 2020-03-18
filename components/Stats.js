5b5aeee9ba7908337bc39ae4f8f295e63ff264b8
irepair <irepair@irepairs-MacBook-Air-2.local>
2020-03-17 21:37:10 -0400

    finished app

---
+++ components/Stats.js
@@ -0,0 +1,45 @@
+import useStats from '../utils/useStats';
+import styled from 'styled-components';
+
+const StatGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+`;
+
+const StatBlock = styled.div`
+  background: #f2f2f2;
+  font-size: 2rem;
+  padding: 2rem;
+  border-radius: 2rem;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+export default function Stats({url}) {
+  const { stats } = useStats(url);
+  if (!stats) return <p>Loading...</p>
+
+  return (
+    <StatGrid>
+      <StatBlock className="statBlock">
+        <h3>Confirmed:</h3>
+        <span>{stats.confirmed.value}</span>
+      </StatBlock>
+
+      <StatBlock className="statBlock">
+        <h3>Deaths:</h3>
+        <span>{stats.deaths.value}</span>
+      </StatBlock>
+
+      <StatBlock className="statBlock">
+        <h3>Recovered:</h3>
+        <span>{stats.recovered.value}</span>
+      </StatBlock>
+
+      <h2>Last Updated: {stats.lastUpdate.substring(0, 10)} @ {stats.lastUpdate.substring(11, 16)}</h2>
+    </StatGrid>
+  )
+}
---
+++ readMe.md
@@ -0,0 +1,5 @@
+# COVID-19 Stat Checker
+
+Global and country statics on confirmed and recovered cases as well as deaths.  Information is gathered using [Mathdro.id's API](https://covid19.mathdro.id/api).
+
+Built using NEXT and React.
